0e8843ddecb15ad7541e501565dc88eeae2001ec
bolinkd <bolinkd@UVic.ca>
2015-06-16 14:17:19 -0700

    Update to p2.c
    Can now run all trains, just needs a scheduler to resolve conflicts

--- p2.c
+++ p2.c
@@ -8,12 +8,15 @@
 struct train{
 	char priority;
 	char direction;
+	char state;
+	//N=new, R=Ready, L=Loading, W=Waiting, C=Crossing, D=Done
 	int loadTime;
 	int waitTime;
 	int trainid;
-	int isComplete;
 	pthread_mutex_t startMutex;
 };
+
+
 
 /*************
 * Struct to hold Processes
@@ -43,50 +46,78 @@ pthread_mutex_t mainTrack;
 pthread_cond_t startCond;
 pthread_attr_t attr;
 
+int countComplete(struct train Trains[],int numTrains){
+	int complete = 0;
+	int i;
+	for(i=0;i<numTrains;i++){
+		if(Trains[i].state == 'D'){
+			complete++;
+		}
+	}
+	return complete;
+}
+
 void *CreateTrain(void *tempTrain){
 	struct train * Train = tempTrain;
-	printf("Hello World! It's me, train #%d!\n", Train->trainid);
-	fflush(stdout);
+	sleep(5);
+	Train->state = 'R';
 	pthread_cond_wait(&startCond, &Train->startMutex);
 	pthread_mutex_unlock(&Train->startMutex);
-	printf("I am Loading: %d\n",Train->trainid);
+	Train->state = 'L';
 	sleep(Train->loadTime);
-	while (pthread_mutex_trylock(&mainTrack) == 0){
+	while (pthread_mutex_lock(&mainTrack) != 0){
 		// DO NOTHING
-		printf("I am Waiting: %d\n",Train->trainid);
-		fflush(stdout);
+		Train->state = 'W';
 	}
-	printf("I am Completing: %d\n",Train->trainid);
-	fflush(stdout);
+	Train->state = 'C';
 	sleep(Train->waitTime);
-	printf("I am Complete: %d\n",Train->trainid);
-	fflush(stdout);
-	Train->isComplete = 1;
+	Train->state = 'D';
 	pthread_mutex_unlock(&mainTrack);
 	pthread_exit(NULL);
 }
 
 void printTrain(struct train toPrint, int trainid){
-	printf("Train %i is headed %c with priority %c after loading for %i and travels for %i\n",trainid,toPrint.direction,toPrint.priority,toPrint.loadTime,toPrint.waitTime);
+	if(toPrint.state == 'N'){
+		printf("Train %i is created\n",trainid);
+	}else if(toPrint.state == 'R'){
+		printf("Train %i is headed %c with priority %c after loading for %i and travels for %i\n",trainid,toPrint.direction,toPrint.priority,toPrint.loadTime,toPrint.waitTime);
+	}else if(toPrint.state == 'L'){
+		printf("Train %i is loading for %i and ready to go %c with priority %c and travels for %i\n",trainid,toPrint.loadTime,toPrint.direction,toPrint.priority,toPrint.waitTime);
+	}else if(toPrint.state == 'C'){
+		printf("Train %i is crossing the track for %i\n",trainid,toPrint.waitTime);
+	}else if(toPrint.state == 'D'){
+		printf("Train %i has crossed the track\n",trainid);
+	}else if(toPrint.state == 'W'){
+		printf("Train %i is waiting to cross the track\n",trainid);	
+	}else{
+		printf("Train %i has state %c and is lost\n",trainid,toPrint.state);
+	}
+	fflush(stdout);
 	
 }
 
 void printTrains(struct train Trains[], int numTrains){
 	int i;
+	printf("------------------------------------------------------------------------\n");
 	for(i=0;i<numTrains;i++){
 		printTrain(Trains[i], i);
 	}
+	printf("------------------------------------------------------------------------\n");
+	fflush(stdout);
 }
 
 void WaitCompletion(struct train Trains[], int numTrains){
-	int i;
-	sleep(10);
-		
+	int i=0;
+	while (i != numTrains){
+		if(Trains[i].state = 'R')
+			i++;
+	}
+	printf("All trains ready to go\n");
+	sleep(5);
 	pthread_cond_broadcast(&startCond);
-	for(i=0;i<numTrains;i++){
-		if(Trains[i].isComplete == 0){
-			i--;	
-		}
+	while (countComplete(Trains,numTrains) != numTrains){
+		printTrains(Trains,numTrains);
+		sleep(2);
 	}
 	return;
 }
@@ -108,7 +139,7 @@ int main(int argc, char *argv[]){
 
 	struct train Trains[numTrains];
 	pthread_t pthreads[numTrains];
-	pthread_mutex_t startMutexes[numTrains];
+
 
 	//init attr
 	pthread_attr_init(&attr);
@@ -119,6 +150,7 @@ int main(int argc, char *argv[]){
 	//init mutex
 	pthread_mutex_init(&mainTrack, NULL);	
 	
+
 
 	//Create All Trains
 	for(i=0;i<numTrains;i++){
@@ -139,8 +171,7 @@ int main(int argc, char *argv[]){
 		Trains[i].loadTime = fileLoadTime;
 		Trains[i].waitTime = fileWaitTime;
 		Trains[i].trainid = i;
-		Trains[i].isComplete = 0;
-		pthread_mutex_init(&(startMutexes[i]),NULL);
+		Trains[i].state = 'N';
 		int rc = pthread_create(&pthreads[i], NULL, CreateTrain, (void *)current);
 		if (rc){
         		printf("ERROR; return code from pthread_create() is %d\n", (int)rc);
@@ -150,6 +181,7 @@ int main(int argc, char *argv[]){
 	
 	printTrains(Trains,numTrains);
 	WaitCompletion(Trains,numTrains);
+	printTrains(Trains,numTrains);
 	for(i=0;i<numTrains;i++){
 		pthread_join(pthreads[i],&Status);
 	}
